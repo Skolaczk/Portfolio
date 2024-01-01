4ab07de9340166f10f983e44745ea066db220485
Michał Skolak <76774237+Skolaczk@users.noreply.github.com>
2024-01-01 15:11:02 +0100

    Remove gallery modal (#3)

--- src/templates/MainTemplate/MainTemplate.js
+++ src/templates/MainTemplate/MainTemplate.js
@@ -4,16 +4,13 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'assets/styles/GlobalStyles';
 import { theme } from 'assets/styles/theme';
 import 'assets/styles/normalize.css';
-import { ModalProvider } from '../../hooks/useModal';
 import { WindowProvider } from '../../hooks/useWindow';
 
 export const MainTemplate = ({ children }) => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <WindowProvider>
-        <ModalProvider>{children}</ModalProvider>
-      </WindowProvider>
+      <WindowProvider>{children}</WindowProvider>
     </ThemeProvider>
   );
 };

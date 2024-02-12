87148cf940c3215db9516b3cec66d58b72f5aeba
Michał <skolakmichal1@gmail.com>
2024-02-12 21:13:17 +0100

    feat: add section divider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ import { ThemeToggle } from '@/components/theme-toggle';
 const Home = () => {
   return (
     <>
-      <div className="container">
+      <div className="container flex flex-col items-center">
         <Intro />
+        <div className="bg-secondary h-16 w-1 rounded-full" />
       </div>
       <div className="fixed bottom-5 right-5 sm:bottom-8 sm:right-8">
         <ThemeToggle />

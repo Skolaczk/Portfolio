af8f2069b089002b14d6aba282fb43912ce4cc5c
Michał Skolak <skolakmichal1@gmail.com>
2024-02-12 20:05:15 +0100

    feat: create reusable button component (#17)
    * feat: add reusable button component and implement it to theme toggle
    
    * fix: change link in PR template

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -2,18 +2,21 @@
 
 import { useTheme } from 'next-themes';
 
+import { Button } from '@/components/button';
 import { Icons } from '@/components/icons';
 
 export const ThemeToggle = () => {
   const { theme, setTheme } = useTheme();
 
   return (
-    <button
+    <Button
+      variant="secondary"
+      size="icon"
       aria-label="theme toggle"
       onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
     >
       <Icons.sun className="dark:hidden" />
       <Icons.moon className="hidden dark:block" />
-    </button>
+    </Button>
   );
 };

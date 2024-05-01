4786f7676d87e77f3a34508416bd3e06b33bdf59
Michał <skolakmichal1@gmail.com>
2024-05-01 20:34:49 +0200

    feat: update cv and experience data

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -75,14 +75,21 @@ export const experiencesData = [
     title: 'Internship at WEB-BOX',
     location: 'Tarnobrzeg, PL',
     description:
-      'During this internship, I created Wordpress plugins and managed website content and settings.',
+      'During this internship, I focused on two key areas: creating Wordpress plugins and managing website content, plugins, and settings.',
     date: '2023',
+  },
+  {
+    title: 'Internship at Chop-Chop',
+    location: 'Remote',
+    description:
+      'During my internship at Chop-Chop, I collaborated with an experienced IT team, focusing on tasks in Next.js. This experience provided valuable insights into teamwork and advanced my proficiency in Next.js.',
+    date: '2024',
   },
   {
     title: 'Freelancing',
     location: 'Remote',
     description:
-      "With a background in graphic design utilizing Figma and web development skills in Next.js with Sanity CMS integration, I bring a hands-on approach to problem-solving and a commitment to meeting clients' unique requirements.",
+      'As a freelancer, I specialized in graphic design using Figma and website development using Next.js with Sanity CMS. I undertook projects independently, from design conceptualization to final implementation, ensuring client satisfaction.',
     date: '2023 - present',
   },
 ] as const;

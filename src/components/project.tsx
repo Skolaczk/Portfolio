2a699f393fe55fd6fab0e701db38cf1151b798fd
Michał Skolak <skolakmichal1@gmail.com>
2024-09-09 20:08:08 +0200

    feat: improve projects section UI (#49)

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -41,38 +41,40 @@ export const Project = ({ project, index, starsCount }: TProps) => {
         once: true,
       }}
       custom={index}
-      className="bg-secondary rounded p-5 md:w-1/3"
+      className="bg-secondary flex flex-col items-center rounded p-5 text-center md:w-1/3"
     >
       <div className="bg-muted w-fit rounded-full p-4">
         <Image src={image} alt={`${title} image`} width={32} height={32} />
       </div>
       <h3 className="my-2 text-lg font-medium">{title}</h3>
       <p className="text-muted-foreground">{description}</p>
-      <div className="my-3 flex flex-wrap gap-2">
+      <div className="my-3 flex flex-wrap justify-center gap-2">
         {technologies.map((tech) => (
           <span className="bg-muted rounded-full px-3 py-1 text-sm" key={tech}>
             {tech}
           </span>
         ))}
       </div>
-      <Button variant="outline" asChild className="mr-2 px-5">
-        <a href={links.preview} aria-label="preview project">
-          <Icons.preview className="size-5" />
-        </a>
-      </Button>
-      <Button variant="outline" asChild className="mr-2 px-5">
-        <a href={links.github} aria-label="github">
-          <Icons.githubOutline className="size-5" />
-        </a>
-      </Button>
-      {starsCount[index] > 100 && (
-        <Button asChild className="px-5">
-          <a href={links.github} aria-label="github">
-            <Icons.star className="mr-2 size-5" />
-            <span className="font-bold">{starsCount[index]}</span>
+      <div className="mt-2 flex">
+        <Button variant="outline" asChild className="mr-2 px-5">
+          <a href={links.preview} aria-label="preview project">
+            <Icons.preview className="size-5" />
           </a>
         </Button>
-      )}
+        <Button variant="outline" asChild className="mr-2 px-5">
+          <a href={links.github} aria-label="github">
+            <Icons.githubOutline className="size-5" />
+          </a>
+        </Button>
+        {starsCount[index] > 100 && (
+          <Button asChild className="px-5">
+            <a href={links.github} aria-label="github">
+              <Icons.star className="mr-2 size-5" />
+              <span className="font-bold">{starsCount[index]}</span>
+            </a>
+          </Button>
+        )}
+      </div>
     </motion.div>
   );
 };

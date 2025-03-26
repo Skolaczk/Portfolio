bae67b453bb5101fc9b78df77cecfa91962bf565
Michał Skolak <skolakmichal1@gmail.com>
2025-03-26 18:11:28 +0100

    fix: fix next-themes props types

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+import { ComponentProps } from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { type ThemeProviderProps } from 'next-themes/dist/types';
 
-export const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
+export function ThemeProvider({
+  children,
+  ...props
+}: ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-};
+}
